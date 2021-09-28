fe93f516b092850aac7ece13546ecf52d1808f2e
Tristan <tristan.hoermann@edu.fhdw.de>
2021-09-28 13:36:21 +0200

    Added Documentation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,12 @@
 import helloWorld.HelloWorld
 
+/**
+ * Main-class.
+ */
 object Main {
+    /**
+     * Main-method.
+     */
     def main(args: Array[String]): Unit = {
         println(HelloWorld.getHelloWorld)
     }
--- src/main/scala/helloWorld/HelloWorld.scala
+++ src/main/scala/helloWorld/HelloWorld.scala
@@ -1,8 +1,15 @@
 package helloWorld
 
+/**
+ * HelloWorld Singleton.
+ */
 object HelloWorld {
     val HELLO_WORLD: String = "Hello World!"
 
+    /**
+     * Returns a Hello World Statement.
+     * @return "Hello World!"
+     */
     def getHelloWorld: String = {
         HELLO_WORLD
     }
