9d75e549d613f9bb9ad4e6b35d69d0cd7c7d83e1
Tristan <triddel02@gmx.de>
2021-11-02 12:45:24 +0100

    Added Fraction.scala class.

---
+++ src/main/scala/PracticeDataStructure/Fraction.scala
@@ -0,0 +1,120 @@
+package PracticeDataStructure
+
+class Fraction(numerator: Int, denominator: Int) {
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param scalar Scalar that should be added.
+     */
+    def add(scalar: Int): Fraction = {
+        new Fraction(numerator + scalar * denominator, denominator)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param frac Fraction that should be added.
+     */
+    def add(frac: Fraction): Fraction = {
+        new Fraction(numerator * frac.denominator + denominator * frac.numerator,
+            denominator * frac.denominator)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param scalar Scalar that should be subtracted.
+     */
+    def subtract(scalar: Int): Fraction = {
+        new Fraction(numerator - scalar * denominator, denominator)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param frac Fraction that should be subtracted.
+     */
+    def subtract(frac: Fraction): Fraction = {
+        new Fraction(numerator * frac.denominator - denominator * frac.numerator,
+            denominator * frac.denominator)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param scalar Scalar the fraction should be multiplied with.
+     */
+    def multiply(scalar: Int): Fraction = {
+        new Fraction(numerator * scalar, denominator)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param frac Fraction the fraction should be multiplied with.
+     */
+    def multiply(frac: Fraction): Fraction = {
+        new Fraction(numerator * frac.numerator, denominator * frac.denominator)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param scalar Scalar the fraction should be divide by.
+     */
+    def divide(scalar: Int): Fraction = {
+        new Fraction(numerator, denominator * scalar)
+    }
+    
+    /**
+     * Returns a new fraction object with the resulting value.
+     *
+     * @param frac Fraction the fraction should be divide by.
+     */
+    def divide(frac: Fraction): Fraction = {
+        new Fraction(numerator * frac.denominator, denominator * frac.numerator)
+    }
+    
+    /**
+     * Returns the fraction as double value.
+     */
+    def toDouble: Double = {
+        numerator.toDouble / denominator
+    }
+    
+    /**
+     * Returns the fraction as float value.
+     */
+    def toFloat: Float = {
+        numerator.toFloat / denominator
+    }
+    
+    /**
+     * Returns a string representation of the object.
+     */
+    override def toString: String = s"$numerator/$denominator"
+    
+    /**
+     * Overrides equals and checks for same type and same numerator and same denominator.
+     */
+    override def equals(obj: Any): Boolean = {
+        obj.isInstanceOf[Fraction] &&
+            obj.asInstanceOf[Fraction].numerator == numerator &&
+            obj.asInstanceOf[Fraction].denominator == denominator
+    }
+    
+    /**
+     * Checks if the fractions have identical numerators and denominators.
+     */
+    def equalsIgnoreValue(frac: Fraction): Boolean = {
+        numerator == frac.numerator && denominator == frac.denominator
+    }
+    
+    /**
+     * Checks if the values represented are equal.
+     */
+    def equalsWithValue(frac: Fraction): Boolean = {
+        toDouble == frac.toDouble
+    }
+}
