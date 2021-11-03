7e0e38c8b2d0cefc9da0d54c9032392b19c1e263
Tristan <triddel02@gmx.de>
2021-11-03 11:09:28 +0100

    Added tests for bubble and quicksort and Array sum and isSorted

--- src/test/scala/Practice3/PracticeThreeTest.scala
+++ src/test/scala/Practice3/PracticeThreeTest.scala
@@ -29,6 +29,57 @@ class PracticeThreeTest extends AnyFunSuite {
     test("Array(1,2,3) = 6"){
         assert(sumOfRec(Array(1,2,3)) == 6 && sumOf(Array(1,2,3)) == 6)
     }
+    test("Array(0) = 0"){
+        assert(sumOfRec(Array(0)) == 0 && sumOf(Array(0)) == 0)
+    }
+    test("Array() = 0"){
+        assert(sumOfRec(Array()) == 0 && sumOf(Array()) == 0)
+    }
+    
+    test("isSorted Array(1,2,3)"){
+        assert(isSorted(Array(1,2,3)))
+    }
+    test("isSorted Array(1,4,3)"){
+        assert(!isSorted(Array(1,4,3)))
+    }
+    
+    test("bubble (1,2,4,3)"){
+        assert(bubbleSort(Array(1, 2, 4, 3)) sameElements Array(1, 2, 3, 4))
+    }
+    test("bubble (1,2,3,4)"){
+        assert(bubbleSort(Array(1, 2, 4, 3)) sameElements Array(1, 2, 3, 4))
+    }
+    test("bubble ()"){
+        assert(bubbleSort(Array[Int]()) sameElements Array[Int]())
+    }
+    test("bubble (4,3,2,1)"){
+        assert(bubbleSort(Array(4,3,2,1)) sameElements Array(1,2,3,4))
+    }
+    test("bubble (4,3)"){
+        assert(bubbleSort(Array(4,3)) sameElements Array(3,4))
+    }
+    test("bubble (4)"){
+        assert(bubbleSort(Array(4)) sameElements Array(4))
+    }
+    
+    test("quickSort (1,2,4,3)"){
+        assert(quickSort(Array(1, 2, 4, 3)) sameElements Array(1, 2, 3, 4))
+    }
+    test("quickSort (1,2,3,4)"){
+        assert(quickSort(Array(1, 2, 4, 3)) sameElements Array(1, 2, 3, 4))
+    }
+    test("quickSort ()"){
+        assert(quickSort(Array[Int]()) sameElements Array[Int]())
+    }
+    test("quickSort (4,3,2,1)"){
+        assert(quickSort(Array(4,3,2,1)) sameElements Array(1,2,3,4))
+    }
+    test("quickSort (4,3)"){
+        assert(quickSort(Array(4,3)) sameElements Array(3,4))
+    }
+    test("quickSort (4)"){
+        assert(quickSort(Array(4)) sameElements Array(4))
+    }
     
     test("Array(1,2,3) equalsButSorted Array(2,1,3)"){
         assert(equalsButSorted(Array(1,2,3), Array(2,1,3)))
