f722cec9dd544479a0c8d8af0c9a62ada172798b
Tristan <triddel02@gmx.de>
2021-11-04 09:08:58 +0100

    Refactored PracticeThree.scala after moving sorting algorithms

--- src/main/scala/Practice3/PracticeThree.scala
+++ src/main/scala/Practice3/PracticeThree.scala
@@ -93,33 +93,13 @@ object PracticeThree {
     Task 9: Bubble und Insertion Sort
      */
     def bubbleSort(unsortedArray: Array[Int]): Array[Int] = {
-        if (unsortedArray.length < 2) return unsortedArray
-        
-        val arrayToSort = unsortedArray.clone()
-        for (i <- arrayToSort.indices;
-             j <- 0 until arrayToSort.length - 1 - i) {
-                if (arrayToSort(j) > arrayToSort(j + 1)) {
-                    val mem: Int = arrayToSort(j)
-                    arrayToSort(j) = arrayToSort(j + 1)
-                    arrayToSort(j + 1) = mem
-                }
-        }
-        arrayToSort
+        PracticeAlgorithms.SortingAlgorithms.bubbleSort(unsortedArray)
     }
     
     /*
     Task 10
      */
     def quickSort(arrayToSort: Array[Int]): Array[Int] = {
-        if (arrayToSort.length <= 1) {
-            arrayToSort
-        }
-        else {
-            val pivot = arrayToSort(arrayToSort.length / 2)
-            Array.concat(
-                quickSort(arrayToSort.filter(pivot > _)),
-                arrayToSort.filter(pivot == _),
-                quickSort(arrayToSort.filter(pivot < _)))
-        }
+        PracticeAlgorithms.SortingAlgorithms.quickSort(arrayToSort)
     }
 }
