8b589fa5e4564af7b6f82b2a92b31862f576fdc8
Tristan <triddel02@gmx.de>
2021-11-04 10:58:05 +0100

    Added Polygon trait

---
+++ src/main/scala/PracticeDataStructure/Polygon.scala
@@ -0,0 +1,18 @@
+package PracticeDataStructure
+
+trait Polygon {
+    /**
+     * Calculates the perimeter of the Polygon.
+     */
+    def perimeter(): Double
+    
+    /**
+     * Calculates the length of all sides individually.
+     */
+    def lengthOfSides(): Any
+    
+    /**
+     * Calculates the area of the polygon.
+     */
+    def area(): Double
+}
