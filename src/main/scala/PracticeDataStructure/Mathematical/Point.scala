07f42ed252359f558b98216f4d443a83c8aaeaa0
Tristan <triddel02@gmx.de>
2021-11-21 12:35:55 +0100

    Moved Point.scala

--- src/main/scala/PracticeDataStructure/Mathematical/Point.scala
+++ src/main/scala/PracticeDataStructure/Mathematical/Point.scala
@@ -1,4 +1,4 @@
-package PracticeDataStructure
+package PracticeDataStructure.Mathematical
 
 /**
  * Point-class. Default coordinates set to origin 0|0
@@ -12,4 +12,10 @@ case class Point(x: Double = 0, y: Double = 0) {
         Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2))
     }
     
+    override def toString: String = this match {
+        case Point(x, 0) => s"Point on x-axis at $x"
+        case Point(0, y) => s"Point on y-axis at $y"
+        case Point(x, y) if x == y => s"Point on diagonal at ($x,$y)"
+        case _ => s"Point($x,$y)"
+    }
 }
