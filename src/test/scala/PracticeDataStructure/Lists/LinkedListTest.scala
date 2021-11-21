37db907b84dc369fefa0a5cb0051ee895d842eae
Tristan <triddel02@gmx.de>
2021-11-21 12:35:01 +0100

    Moved LinkedListTest.scala

---
+++ src/test/scala/PracticeDataStructure/Lists/LinkedListTest.scala
@@ -0,0 +1,31 @@
+package PracticeDataStructure.Lists
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class LinkedListTest extends AnyFunSuite {
+    
+    test("empty null") {
+        assert(new LinkedList[Null](null).isEmpty)
+    }
+    
+    test("not empty") {
+        assert(!new LinkedList[String]("Test").isEmpty)
+    }
+    
+    test("add one and size=1") {
+        assert(new LinkedList[String]("Test").length == 1)
+    }
+    
+    test("Get index 3") {
+        val list: LinkedList[Int] = new LinkedList[Int](1).add(2).add(3).add(4)
+        assert(list.get(3) == 4 && 4 == list(3))
+    }
+    
+    test("append all test") {
+        val list1: LinkedList[Int] = new LinkedList[Int](1).add(2).add(3).add(4) //1,2,3,4
+        val list2: LinkedList[Int] = new LinkedList[Int](2).add(4) //2,4
+        assert(list1.addAll(list2).length == 6)
+        assert(list1.iterator.sameElements(new LinkedList[Int](1).add(2).add(3).add(4).add(2).add(4)))
+    }
+}
+
