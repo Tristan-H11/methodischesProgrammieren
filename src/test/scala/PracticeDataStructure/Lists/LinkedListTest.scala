2ad3af254af364a4a7bb43e1045030820575df35
Tristan <triddel02@gmx.de>
2021-11-21 18:36:33 +0100

    Improved LinkedListTest.scala

--- src/test/scala/PracticeDataStructure/Lists/LinkedListTest.scala
+++ src/test/scala/PracticeDataStructure/Lists/LinkedListTest.scala
@@ -4,12 +4,12 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class LinkedListTest extends AnyFunSuite {
     
-    test("empty null") {
-        assert(new LinkedList[Null](null).isEmpty)
+    test("empty") {
+        assert(new LinkedList[Int]().isEmpty)
     }
     
     test("not empty") {
-        assert(!new LinkedList[String]("Test").isEmpty)
+        assert(!(new LinkedList[String]("Test").isEmpty))
     }
     
     test("add one and size=1") {
@@ -26,6 +26,12 @@ class LinkedListTest extends AnyFunSuite {
         val list2: LinkedList[Int] = new LinkedList[Int](2).add(4) //2,4
         assert(list1.addAll(list2).length == 6)
         assert(list1.iterator.sameElements(new LinkedList[Int](1).add(2).add(3).add(4).add(2).add(4)))
+    }
+    
+    test("remove 2 from 1,2,3"){
+        val list1 = new LinkedList[Int]().add(1).add(2).add(3)
+        list1.remove(2)
+        assert(list1 == new LinkedList[Int](1).add(2))
     }
 }
 
