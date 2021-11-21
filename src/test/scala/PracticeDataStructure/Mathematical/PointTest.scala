638d5fb401890bb1d324d992adf47836d90471c2
Tristan <triddel02@gmx.de>
2021-11-21 12:36:04 +0100

    Moved PointTest.scala

--- src/test/scala/PracticeDataStructure/Mathematical/PointTest.scala
+++ src/test/scala/PracticeDataStructure/Mathematical/PointTest.scala
@@ -1,4 +1,4 @@
-package PracticeDataStructure
+package PracticeDataStructure.Mathematical
 
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -29,4 +29,16 @@ class PointTest extends AnyFunSuite{
         assert(Point(1,1).distance(Point(-2,5)) == 5.0)
     }
     
+    test("on x-axis"){
+        assert(Point(2).toString =="Point on x-axis at 2.0")
+    }
+    test("on y-axis"){
+        assert(Point(0,2).toString =="Point on y-axis at 2.0")
+    }
+    test("on diagonal"){
+        assert(Point(2,2).toString =="Point on diagonal at (2.0,2.0)")
+    }
+    test("other point"){
+        assert(Point(2,3).toString =="Point(2.0,3.0)")
+    }
 }
